133cc821dec7da9d21d920ac91ceac44c6fc9962
Sasha <petrov.knu@gmail.com>
2017-05-19 12:05:11 +0200

    creational/abstract-factory

--- creational/README.md
+++ creational/README.md
@@ -4,8 +4,19 @@
 **TODO:**
 
 
-- [ ] Factory method
-- [ ] Abstract factory
+- [x] Factory method
+- [x] Abstract factory
 - [ ] Builder
 - [ ] Prototype
 - [ ] Singleton
+
+
+### Factory method 
+
+
+![](https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/New_WikiFactoryMethod.png/734px-New_WikiFactoryMethod.png)
+
+
+
+### Abstract factory 
+![](https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Abstract_factory_UML.svg/677px-Abstract_factory_UML.svg.png)
---
+++ creational/abstract-factory.cc
@@ -0,0 +1,133 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+
+// Abstract product A
+class Button
+{
+public:
+    virtual void click() const = 0;
+};
+
+class Window
+{
+public:
+    virtual void show() const = 0;
+};
+
+
+// Abstract product B
+class OsxButton: public Button
+{
+public:
+    virtual void click() const 
+    {
+        cout << "Clicking OSX button: Clack ..." << endl;
+    }
+};
+
+
+// Concrete products A, B
+//
+//
+
+class WinButton: public Button
+{
+public:
+    virtual void click() const 
+    {
+        cout << "Clicking Win button. Click ..." << endl;
+    }
+};
+
+
+class OsxWindow: public Window
+{
+public:
+    virtual void show() const
+    {
+        cout << "Showing OSX window ..." << endl;
+    }
+};
+
+class WinWindow: public Window
+{
+public:
+    virtual void show() const
+    {
+        cout << "Showing Win window ..." << endl;
+    }
+};
+
+
+class AbstractFactory
+{
+public:
+    virtual Button * create_button() const = 0;
+    virtual Window * create_window() const = 0;
+};
+
+
+// Concrete Factory 1
+//
+//
+
+class OsxFactory: public AbstractFactory
+{
+public:
+    virtual Button * create_button() const
+    {
+        return new OsxButton();
+    }
+    virtual Window * create_window() const
+    {
+        return new OsxWindow();
+    }
+};
+
+// Concrete Factory 2
+//
+//
+
+class WinFactory: public AbstractFactory
+{
+public:
+    virtual Button * create_button() const
+    {
+        return new WinButton();
+    }
+    virtual Window * create_window() const
+    {
+        return new WinWindow();
+    }
+};
+
+
+int main(int argc, char const *argv[])
+{
+    // Client code
+
+    int size = 3;
+    AbstractFactory * factories[size] = {new OsxFactory(), new WinFactory(), new OsxFactory()};
+
+    cout << "Creating windows: " << endl << endl;
+    for(int i = 0; i < size; ++i)
+    {
+        Window * w = factories[i]->create_window();
+        w->show();
+        delete w;
+    }
+
+    cout << endl;
+    cout << "Clicking all the buttons: " << endl << endl;
+    for(int i = 0; i < size; ++i)
+    {
+        Button * b = factories[i]->create_button();
+        b->click();
+        delete b;
+    }
+
+    return 0;
+}
